3f8b467aa07b9fb20d605b8265db20c035c433d2
c <shaikchapal@hotmail.com>
2023-11-07 00:18:35 +0600

    add-project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,23 +10,14 @@ const Footer = () => {
             <div className="flex h-full flex-col justify-between">
               <div className="mb-4 inline-flex items-center w-20 md:w-30">
                 <img src={logo} alt="logo" />
-                <span className="ml-4 text-lg font-bold">Hotel Mome</span>
+                <span className="ml-4 text-lg font-bold">Hotel Chapal</span>
               </div>
-              <div>
-                <p className="mb-4  text-base font-medium">
-                Made with ❤️ by Jaimin Patel
-                </p>
-                <p className="text-sm">
-                  &copy; Copyright 2022. All Rights Reserved by Hotel Momo.
-                </p>
-              </div>
+          
             </div>
           </div>
           <div className="w-full p-6 md:w-1/2 lg:w-2/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase">
-                Company
-              </h3>
+           
               <ul>
                 <li className="mb-4">Features</li>
                 <li className="mb-4">Pricing</li>
@@ -36,9 +27,7 @@ const Footer = () => {
           </div>
           <div className="w-full p-6 md:w-1/2 lg:w-2/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase ">
-                Support
-              </h3>
+              
               <ul>
                 <li className="mb-4">Account</li>
                 <li className="mb-4">Help</li>
@@ -48,9 +37,7 @@ const Footer = () => {
           </div>
           <div className="w-full p-6 md:w-1/2 lg:w-3/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase ">
-                Legals
-              </h3>
+             
               <ul>
                 <li className="mb-4">Terms &amp; Conditions</li>
                 <li className="mb-4">Privacy Policy</li>
